b0ef10a4e2a97c12bd2a57c2f3e2b80da2c15f10
ksnb <4644sheng@sina.com>
2020-09-24 15:56:48 +0800

    登录功能

--- js/login.js
+++ js/login.js
@@ -1,3 +1,34 @@
+// 获取请求参数，用户mock funtion来判断返回
+function getParam (url) {
+    console.log(url, '---->url');
+    let param ={};
+    if(typeof url !== 'string') {
+        return new Error('function need a String type param');
+    }
+
+    url.split('?')[1]
+        .split('&').forEach(item => {
+            param[item.split('=')[0]] = item.split('=')[1];
+        });
+    return param;
+}
+// 定义验证策略
+const strategy = (options) => {
+    const { loginType } = options;
+    if (!loginType) throw Error('give me a loginType');
+    if (loginType === '1') {
+        const { username } = options;
+        const { password } = options;
+        if (!username) return { result: false, message: '请输入用户名/邮箱' };
+        if (!password) return { result: false, message: '请输入密码' };
+        return { result: true, message: 'success' };
+    } else {
+        const { preNumber, sufNumber } = options;
+        if (!preNumber) return { result: false, message: '请添加区号' };
+        if (!sufNumber) return { result: false, message: '请添加号码' };
+        return { result: true, message: 'success' };
+    }
+};
 if (window.environment === 'web') {
     (function() {
         // web login  -login页面显示关闭
@@ -43,23 +74,7 @@ if (window.environment === 'web') {
             this.previousElementSibling.className = 'username-login click';
             loginType = '2';
         }, false);
-        // 3、定义验证策略
-        const strategy = (options) => {
-            const { loginType } = options;
-            if (!loginType) throw Error('give me a loginType');
-            if (loginType === '1') {
-                const { username } = options;
-                const { password } = options;
-                if (!username) return { result: false, message: '请输入用户名/邮箱' };
-                if (!password) return { result: false, message: '请输入密码' };
-                return { result: true, message: 'success' };
-            } else {
-                const { preNumber, sufNumber } = options;
-                if (!preNumber) return { result: false, message: '请添加区号' };
-                if (!sufNumber) return { result: false, message: '请添加号码' };
-                return { result: true, message: 'success' };
-            }
-        };
+
 
         // 4、 登录功能
 
@@ -95,15 +110,21 @@ if (window.environment === 'web') {
                 /127\.0\.0\.1:8085\/login/,
                 'get',
                 function({ url, type, body }) {
-                    console.log(url, '--->url');
-                    console.log(type, '--->type');
-                    console.log(body, '--->body');
-                    return {
-                        code: 200,
-                        data: {
-                            message: 'success'
-                        }
-                    };
+                    var param = getParam(url);
+                    if(param.username === 'lilei' && password === '111') {
+
+                        return Mock.mock({
+                            'boolean|1': true
+                        });
+
+                    } else {
+                        return JSON.stringify({
+                            code: 300,
+                            data: {
+                                message: 'password or username is not correct'
+                            }
+                        });
+                    }
                 }
 
             );
@@ -111,7 +132,11 @@ if (window.environment === 'web') {
                 url: 'http://127.0.0.1:8085/login?username=lilei&password=111',
                 method: 'GET',
                 success: function(res) {
-                    console.log(res, '------->');
+                    console.log( res.boolean);
+                    if(res.code == 200) {
+                        alert('登录成功');
+                        colseBtn.click();
+                    }
                 },
                 error: function(err) {
                     console.log(err);
